0d28970d37fa4a36c8d6a81ccda661590c10cff9
clive <98222803+AAClive@users.noreply.github.com>
2022-06-28 22:23:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,19 +14,51 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
 from flask_sockets import Sockets
 import asyncio
-class Sock(db.Model)
+import datetime
+
+r=[]
+db=SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"]="postgres://fjdytdbpqlotqg:0edde1d5a396977261f0428716b633b773e28bece96617dce516284ae10e6c64@ec2-3-222-74-92.compute-1.amazonaws.com:5432/dp6uugc3h8q26"
+x=datetime.datetime.now()
+x
+class Sock(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    date=db.Column(db.String(255))
+    num5=db.Column(db.String(255))
+    num4=db.Column(db.String(255))
+    num3=db.Column(db.String(255))
+    num2=db.Column(db.String(255))
+    num1=db.Column(db.String(255))
+
+    
 @app.route("/")
 def home():
     return "E"
 async def e(websocket,path):
     await websocket.send("[CLIENT]")
-    print("FEIAOFNIAOFNWIAONFWIAOFNWIAOFNWIAOFNWIAOFNWIAOFNWIAONFWIAONFWIAONFWIAONFWIAOFNWIAOFNWIAOFNWIAONFWIAOFNWOANFIWAONFWNIOAWFIONWIFAONAIFOWNIAWFONIFWANIOFNIAOFWNIWANIOFAWNIOFNWIOANIOFWNAIOFNIWOANFIOWANIOFWNIAOFNIOWANIOFWNAIOFNIWOANIOFWNIAOFNWIOA")
     async for message in websocket:
         print(f"[MSG] : {message}")
         await websocket.send(f"[BACK] {message}")
-        await websocket.send(f"[BACK] {message}")
-        await websocket.send(f"[BACK] {message}")
-        await websocket.send(f"[BACK] {message}")
+        if message=="[DONE]":
+            num1=min(r)
+            comwda=r
+            comwda.remove(num1)
+            num2=min(r)
+            comwda.remove(num2)
+            num3=min(r)
+            comwda.remove(num3)
+            num4=min(r)
+            comwda.remove(num4)
+            num5=min(r)
+            comwda.remove(num5)
+            xd=datetime.datetime.now()
+            xd=str(xd)
+            xd=xd.split(" ")
+            xd=xd[0]
+            new_req=Sock(date=xd,num5=num5,num4=num4,num3=num3,num2=num2,num1=num1)
+        else:
+            r.append(message)
+        
 start_server = websockets.serve(e, '0.0.0.0', os.environ['PORT'])
 asyncio.get_event_loop().run_until_complete(start_server)
 if __name__=="__main__":
