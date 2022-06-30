f205ae88635ad9a00aa25f8883018b884102b97c
clive <98222803+AAClive@users.noreply.github.com>
2022-06-30 09:29:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ import datetime
 r=[]
 db=SQLAlchemy(app)
 app.config["SQLALCHEMY_DATABASE_URI"]="postgres://fjdytdbpqlotqg:0edde1d5a396977261f0428716b633b773e28bece96617dce516284ae10e6c64@ec2-3-222-74-92.compute-1.amazonaws.com:5432/dp6uugc3h8q26"
+from sqlalchemy import create_engine
+
 x=datetime.datetime.now()
 x
 class Sock(db.Model):
@@ -56,6 +58,7 @@ async def e(websocket,path):
             xd=xd.split(" ")
             xd=xd[0]
             new_req=Sock(date=xd,num5=num5,num4=num4,num3=num3,num2=num2,num1=num1)
+            db.session.commit()
             await websocket.send("[SERVER] prime")
         else:
             r.append(message)
